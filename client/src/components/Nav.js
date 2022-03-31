7e26b4ad04a4b2c5a0a9a2bde6aac90be31e0f3c
Jen Truong <jennifertruong09@gmail.com>
2022-03-31 15:07:35 -0400

    added logo and nav

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -2,15 +2,16 @@
 // Tinder uses two logos, a plain logo for the larger page, and a color logo for the minizimed/mobile layout
 
 import smallLogo from '../images/colorLogoPlaceholder.jpg'
-import bigLogo from '../images/plainLogoPlaceholder.jpg'
+import bigLogo from '../images/honeylogo.png'
 
-const Nav = ({ mobile, authToken, setShowBuild, showBuild, setIsSignUp }) => {
+const Nav = ({ props, mobile, authToken, setShowBuild, showBuild, setIsSignUp }) => {
     
     const handleClick = () => {
         setShowBuild(true)
         setIsSignUp(false)
     }
-    
+
+    const tabs = ['dash', 'room']
     
     return (
         <nav>
@@ -18,6 +19,16 @@ const Nav = ({ mobile, authToken, setShowBuild, showBuild, setIsSignUp }) => {
                 <img className="logo" src={mobile ? smallLogo : bigLogo} alt="Company Logo" />
                 
             </div>
+            <ul className='navBar'>
+            {tabs.map(tab => (
+                <li className='navItem' key={tab}>
+                    {/* // ternary operator for link to page depending on which page user is on -- fix later if issues? */}
+                    <a href={tab} onClick={() => props.handlePageChange(tab)}>
+                        {tab.charAt(0).toUpperCase() + tab.slice(1)}
+                    </a>
+                </li>
+            ))}
+        </ul>
             {!authToken && !mobile  && <button 
             className="navBtn" 
             onClick={handleClick}
