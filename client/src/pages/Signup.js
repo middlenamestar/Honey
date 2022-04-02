10921af80e9ebd20e8a089423eab74740ed4eb03
priscilla ma <priscillama621@gmail.com>
2022-04-01 23:24:45 -0400

    added textarea resize none style, changed variable names, added comments, styled a basic layout using bootstrap.

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,18 +1,25 @@
 import { useState } from 'react';
 import Nav from '../components/Nav';
 import ProfilePic from '../components/uploadPic';
+import '../styles/signup.css';
+
+const styles = {
+    txtAreaResize: {
+        resize: 'none'
+    }
+};
 
 const Signup = () => {
     
     const [formData, setFormData] = useState({
-        userName: '',
-        malUserName: '',
-        dobDay: '',
-        dobMonth: '',
-        dobYear: '',
+        username: '',
+        malUsername: '',
+        bdayMo: '',
+        bdayDay: '',
+        bdayYear: '',
         bio: '',
-        email: '',
-        imageUrl: '',
+        // email: '',
+        // imageUrl: '',
         matches: []
     })
     
@@ -31,101 +38,140 @@ const Signup = () => {
             [name] : value
         }))
     }
-    console.log(formData)
+    console.log(formData);
     
     return (
         <>
-        <Nav mobile={false}
-        setShowAuth={() => {}}
-        showAuth={false}
-        />
-            <div className='signup'>
-                <h2>Create Account</h2>
-                
-                <form onSubmit={handleSubmit}>
-                    <section>
-                        <label htmlFor='userName'>User Name </label>
-                        <input
-                        id='userName'
-                        type='text'
-                        name='userName'
-                        placeholder='User Name'
-                        required={true}
-                        value={formData.userName}
-                        onChange={handleChange}
-                        />
-                        
-                        <label htmlFor='malUserName'>MyAnimeList User Name </label>
-                        <input
-                        id='malUserName'
-                        type='text'
-                        name='malUserName'
-                        placeholder='MyAnimeList User Name'
-                        required={true}
-                        value={formData.malUserName}
-                        onChange={handleChange}
-                        /> 
-                                                                                          
-                        <label>Birthday </label>
-                        <div className='birthdayInputContainer'>
-                        <input
-                        id='dobDay'
-                        type='number'
-                        name='dobDay'
-                        placeholder='DD'
-                        required={true}
-                        value={formData.dobDay}
-                        onChange={handleChange}
-                        />   
-                        <input
-                        id='dobMonth'
-                        type='number'
-                        name='dobMonth'
-                        placeholder='MM'
-                        required={true}
-                        value={formData.dobMonth}
-                        onChange={handleChange}
-                        />     
-                             
-                        <input
-                        id='dobYear'
-                        type='number'
-                        name='dobYear'
-                        placeholder='YYYY'
-                        required={true}
-                        value={formData.dobYear}
-                        onChange={handleChange}
-                        />
-                        </div>
-                        
-                        <label htmlFor='bio'>About Me </label>
-                        <input
-                        id='bio'
-                        type='text'
-                        name='bio'
-                        placeholder='Tell us about yourself!'
-                        required={true}
-                        value={formData.bio}
-                        onChange={handleChange}
-                        />
-                        <input  type='submit' />
-                    </section> 
-                          
-                    <section>                 
-                        <p>PROFILE PIX.</p>
-                        <ProfilePic/>
-                        {/* <input
-                        type='url'
-                        name='imageUrl'
-                        id='imageUrl'
-                        onChange={handleChange}
-                        required={true}
-                        /> */}
-                        {/* <div className='photoContainer'>
-                            <img src={formData.imageUrl} alt='profile pic'/>
-                        </div> */}
-                    </section>
-                </form>
+            {/* nav component. */}
+            <Nav mobile={false}
+            setShowAuth={() => {}}
+            showAuth={false}
+            />
+
+            {/* container/formatting */}
+            <div className='container'>
+                <div className='row'>
+                    {/* col to make the entire form vertical. */}
+                    <div className='col'>
+
+                        {/* header */}
+                        <h2>build profile</h2>
+
+                        {/* start of form. */}
+                        <form onSubmit={handleSubmit}>
+
+                            {/* profile pic component. */}
+                            <ProfilePic/>
+
+                            {/* <input
+                            type='url'
+                            name='imageUrl'
+                            id='imageUrl'
+                            onChange={handleChange}
+                            required={true}
+                            /> */}
+                            {/* <div className='photoContainer'>
+                                <img src={formData.imageUrl} alt='profile pic'/>
+                            </div> */}
+
+                            {/* username. */}
+                            <div className='form-group'>
+                                <label htmlFor='username'>username</label>
+                                <input
+                                    id='username'
+                                    type='text'
+                                    name='username'
+                                    className='form-control'
+                                    placeholder='username'
+                                    required={true}
+                                    value={formData.userName}
+                                    onChange={handleChange}
+                                />
+                            </div>
+                            
+                            {/* my anime list username. */}
+                            <div className='form-group'>
+                                <label htmlFor='malUsername'>MYANIMELIST username</label>
+                                <input
+                                    id='malUsername'
+                                    type='text'
+                                    name='malUsername'
+                                    className='form-control'
+                                    placeholder='myanimelist username'
+                                    required={true}
+                                    value={formData.malUserName}
+                                    onChange={handleChange}
+                                />
+                            </div>
+
+                            {/* birthday. */}
+                            <div className='row'>
+
+                                <label>birthday 🎈🦂</label>
+
+                                <div className='col'>
+                                    <input
+                                        id='bdayMo'
+                                        type='number'
+                                        name='bdayMo'
+                                        className='form-control'
+                                        placeholder='mm'
+                                        required={true}
+                                        value={formData.dobDay}
+                                        onChange={handleChange}
+                                    />
+                                </div>
+
+                                <div className='col'>
+                                    <input
+                                        id='bdayDay'
+                                        type='number'
+                                        name='bdayDay'
+                                        className='form-control'
+                                        placeholder='dd'
+                                        required={true}
+                                        value={formData.dobMonth}
+                                        onChange={handleChange}
+                                    />
+                                </div>
+
+                                <div className='col'>
+                                    <input
+                                        id='bdayYear'
+                                        type='number'
+                                        name='bdayYear'
+                                        className='form-control'
+                                        placeholder='yyyy'
+                                        required={true}
+                                        value={formData.dobYear}
+                                        onChange={handleChange}
+                                    />
+                                </div>
+
+                            </div>
+
+                            {/* about section. */}
+                            <div className='form-group'>
+                                <label htmlFor='bio'>about me</label>
+                                <textarea
+                                    id='bio'
+                                    name='bio'
+                                    placeholder='opinions on studio ghibli?'
+                                    className='form-control'
+                                    rows='6'
+                                    style={styles.txtAreaResize}
+                                    required={true}
+                                    value={formData.bio}
+                                    onChange={handleChange}
+                                />
+                            </div>
+
+                            {/* SUBMIT FORM */}
+                            <input type='submit' className='' value='save'/>
+
+                        </form>
+                    </div>
+                </div>
             </div>
         </>
     ) 
