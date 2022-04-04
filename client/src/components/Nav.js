4eee8a0406e70abdeacab22e4bcc1e3f529b2cf4
priscilla ma <priscillama621@gmail.com>
2022-04-04 14:40:47 -0400

    small styles

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,31 +1,13 @@
-import { useCookies } from 'react-cookie';
-import {useNavigate} from 'react-router-dom';
 import { Navbar, Container, Nav, Offcanvas } from 'react-bootstrap';
+import './nav.css';
 
-const Navigation = ({ props, setShowBuild, showBuild, setIsSignUp }) => {
-    const [cookies, setCookie, removeCookie] =useCookies(['user']);
-    const handleClick = () => {
-        setShowBuild(true)
-        setIsSignUp(false)
-    };
+const styles = {
+    bg: {
+        // backgroundColor: "black"
+    }
+}
 
-    let navigate = useNavigate();
-    const LogOuthandleClick  =() =>{
-        removeCookie('UserId', cookies.UserId);
-        removeCookie('AuthToken', cookies.AuthToken);
-        navigate('/');
-    };
-
-    const authToken = cookies.AuthToken
-    const tabs = ['Dash', 'Room', 'Donations Page', 'Profile']
-    
-    const loginFlip = () =>{
-        if(!authToken) {
-            return true;
-        } else {
-            return false;
-        }
-    };
+const Navigation = () => {
 
     return (
         <>
@@ -40,6 +22,7 @@ const Navigation = ({ props, setShowBuild, showBuild, setIsSignUp }) => {
                         id="offcanvasNavbar"
                         aria-labelledby="offcanvasNavbarLabel"
                         placement="end"
+                        style={styles.bg}
                     >
                         <Offcanvas.Header closeButton>
                             <Offcanvas.Title
@@ -47,8 +30,8 @@ const Navigation = ({ props, setShowBuild, showBuild, setIsSignUp }) => {
                         </Offcanvas.Header>
                         <Offcanvas.Body>
                             <Nav className="justify-content-end flex-grow-1 pe-3">
-                                <Nav.Link href="/signup">Profile</Nav.Link>
-                                <Nav.Link href="/dash">Match</Nav.Link>
+                                <Nav.Link href="/profile">Profile</Nav.Link>
+                                <Nav.Link href="/dash">Meet</Nav.Link>
                                 <Nav.Link href="/room">Chatrooms</Nav.Link>
                                 <Nav.Link href="/donations">Donate 🍩</Nav.Link>
                             </Nav>
