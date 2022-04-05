2f33757e72275b2f3f244dd9a5d9f6a84f887bbd
priscilla ma <priscillama621@gmail.com>
2022-04-05 02:21:36 -0400

    lil style on success

--- client/src/pages/Success.jsx
+++ client/src/pages/Success.jsx
@@ -1,21 +1,30 @@
 import React, {useEffect} from 'react'
 import { useHistory } from 'react'
 import useWindowSize from 'react-use/lib/useWindowSize'
-import Confetti from 'react-confetti'
+import Confetti from 'react-confetti';
+import Navigation from "../components/Nav";
+import { Container, Row, Col } from 'react-bootstrap';
 
 const Success = () => {
-    const { width, height } = useWindowSize()
-    useEffect(() => {
-        setTimeout(()=>{
-            window.location = `http://localhost:3000/dash`
-        },8000);
-      }, []);
+  const { width, height } = useWindowSize()
+  useEffect(() => {
+      setTimeout(()=>{
+          window.location = `http://localhost:3000/dash`
+      },8000);
+    }, []);
+
   return (
     <>
-    <Confetti width={width}  height={height}/>
-    <h1>Thank you so much for your support</h1>
+      <Container className='my-4'>
+        <Row className='justify-content-center'>
+          <Col>
+            <Confetti width={width} height={height}/>
+            <h3>Thank you so much for your support! (っ^_^)っ</h3>
+          </Col>
+        </Row>
+      </Container>
     </>
   )
-}
+};
 
-export default Success
+export default Success;
